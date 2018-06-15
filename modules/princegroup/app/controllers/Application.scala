7903906bb093de973913c32f718254839f08c1ec
zberone <zhunumberone@hotmail.com>
2018-06-15 15:51:53 +0800

    all page added

--- modules/princegroup/app/controllers/Application.scala
+++ modules/princegroup/app/controllers/Application.scala
@@ -15,6 +15,29 @@ class Application @Inject() (val messagesApi: MessagesApi, conf: Configuration) 
     Ok(views.html.princegroup.index(Messages("princegroup.subtitle"), computers, configThisFile = conf.underlying.as[String]("this.file")))
   }
 
+  def about = Action { implicit request =>
+    val computers = PrincegroupWeb.list
+    Ok(views.html.princegroup.about(Messages("princegroup.subtitle"), computers, configThisFile = conf.underlying.as[String]("this.file")))
+  }
+
+  def brands(ID: String) = Action { implicit request =>
+    val computers = PrincegroupWeb.list
+    //    if (List("fiveram", "bibi", "pid").contains(ID)) {
+    //      Ok(views.html.princegroup.brands(Messages("princegroup.subtitle"), ID, computers, configThisFile = conf.underlying.as[String]("this.file")))
+    //    }
+    Ok(views.html.princegroup.brands(Messages("princegroup.subtitle"), ID, computers, configThisFile = conf.underlying.as[String]("this.file")))
+  }
+
+  def contact = Action { implicit request =>
+    val computers = PrincegroupWeb.list
+    Ok(views.html.princegroup.contact(Messages("princegroup.subtitle"), computers, configThisFile = conf.underlying.as[String]("this.file")))
+  }
+
+  def csr = Action { implicit request =>
+    val computers = PrincegroupWeb.list
+    Ok(views.html.princegroup.csr(Messages("princegroup.subtitle"), computers, configThisFile = conf.underlying.as[String]("this.file")))
+  }
+
   def selectLang(lang: String) = Action { implicit request =>
     Logger.logger.debug("Change user lang to : " + lang)
     request.headers.get(REFERER).map { referer =>
